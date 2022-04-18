d8b970617d121d5a2b8df9ed88c99d9eab180d56
Rox06 <103920634+Rox06@users.noreply.github.com>
2022-04-18 11:02:36 -0500

    Se agregan carpetas
    Carpetas que complementan la funcionalidad del proyecto en CI4

---
+++ app/Controllers/Libros.php
@@ -0,0 +1,123 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\Libro;
+
+class Libros extends Controller{
+
+    public function index(){
+        
+        $libro = new Libro();
+        $datos['libros'] = $libro->orderBy('id_libro','ASC')->findAll();
+
+        $datos['cabecera'] = view('template/cabecera');
+        $datos['pie'] = view('template/piepagina');
+    
+        return view('libros/listar',$datos);
+    }
+
+    public function crear(){
+
+        $datos['cabecera'] = view('template/cabecera');
+        $datos['pie'] = view('template/piepagina');
+
+        return view('libros/crear',$datos);
+    }
+
+    public function guardar(){
+        
+        $libro = new Libro();
+
+        $nombrelibro=$this->request->getVar('nombrelibro');
+        
+        if($imagenlibro=$this->request->getFile('imagenlibro')){
+            
+            $nuevoImagenlibro=$imagenlibro->getRandomName();
+            $imagenlibro->move('../public/imagenes/',$nuevoImagenlibro);
+           
+            $datos=[
+                'nombre_libro'=>$nombrelibro,
+                'imagen_libro'=>$nuevoImagenlibro
+            ];
+
+            $libro->insert($datos);
+        }
+
+        return $this->response->redirect(site_url('/listar'));
+    }
+
+    public function borrar($id_libro=null){#Null en caso de no recepcionar nada
+
+        $libro = new Libro();
+        #Busca id que coincida con id enviado y agarra el primer registro.
+        $datosLibro=$libro->where('id_libro',$id_libro)->first();
+
+        #Agarra primer registro y se crea ruta de borrado de libro
+        $ruta=('../public/imagenes/'.$datosLibro['imagen_libro']);
+
+        if (file_exists($ruta)) {
+            unlink($ruta);
+        }#Borrado
+        
+        #Borra en BD, respetandi ID enviado
+        $libro->where('id_libro',$id_libro)->delete($id_libro);
+
+        return $this->response->redirect(site_url('/listar'));      
+    }
+
+    public function editar($id_libro=null){
+       
+        $libro = new Libro();
+
+        $datos['libro']=$libro->where('id_libro',$id_libro)->first();
+        #Archivo editar recibira esos datos con variable de nombre $libro
+
+        $datos['cabecera'] = view('template/cabecera');
+        $datos['pie'] = view('template/piepagina');
+
+        return view('libros/editar',$datos);
+    }
+
+    public function actualizar(){
+        var_dump($id_libro=$this->request->getVar('id_libros'));
+        
+        /*
+        $libro = new Libro();
+
+        $datos=[
+            'nombre_libro'=>$this->request->getVar('nombrelibro')
+        ];
+        $idlibro=$this->request->getVar('idlibros'); #idlibros viene de interfaz editar boton hidden
+        
+        $libro->update($idlibro,$datos);
+        */
+        /*
+        $validacion = $this->validate([
+            'imagen_libro'=>[
+                'uploaded[imagen_libro]',
+                'mime_in[imagen_libro,image/jpg,image/jpeg,image/png]',
+                'max_size[imagen_libro,1024]',
+            ]
+        ]);
+
+        if($validacion){
+            if($imagenlibro=$this->request->getFile('imagenlibro')){
+            
+                $datosLibro=$libro->where('id_libro',$idlibro)->first();
+                $ruta=('../public/imagenes/'.$datosLibro['imagen_libro']);
+                unlink($ruta);
+
+                $nuevoImagenlibro=$imagenlibro->getRandomName();
+                $imagenlibro->move('../public/imagenes/',$nuevoImagenlibro);
+
+                $datos=['imagen_libro'=>$nuevoImagenlibro];
+    
+                $libro->update($idlibro,$datos);
+            }
+        }*/
+
+    }
+
+
+}
